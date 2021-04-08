099bb6ca8e4aeab7bb7d1d9e1309456bfb2eeec6
Eriberto Guzman <eriberto.guzman@outlook.com>
2021-04-08 14:14:19 -0400

    Implemented full crud for IMAGE Live Lecture on Rails CRUD, Added bootstrap and Navigations.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -12,11 +12,48 @@ class ImagesController < ApplicationController
     end
     
     def new
-
+        @image = Image.new
     end
 
     def create
-        
+        @image = Image.new(images_params)
+        if @image.save
+            redirect_to image_path(@image)
+        else
+            # implement some error handling
+            # If it doesnt save
+            # @image.errors
+            # TODO: add some flash messaging
+            render :new
+        end
     end
 
+    def edit
+        @image = Image.find_by(id: params[:id] )
+    end
+
+    def update
+        @image = Image.find_by(id: params[:id])
+        @image.update(image_params)
+
+        if @image.valid?
+            redirect_to image_path(@image)
+        else
+            #TODO: ERROR MESSAGES
+            render :edit
+        end
+    end
+
+    def destroy
+        #TODO: DRY THIS UP BRO
+        @image = Image.find_by(id: params[:id])
+        @image.destroy
+        redirect_to images_path
+    end
+
+    private
+    #Strong Params
+    def image_params
+        params.require(:image).permit(:title, :description, :image_url)
+    end
 end
