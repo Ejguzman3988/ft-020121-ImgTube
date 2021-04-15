0749d688c924ccb5689909be4c0daad992f9d5bb
Eriberto Guzman <eriberto.guzman@outlook.com>
2021-04-15 15:07:17 -0400

    Add partials and a bit of styling. Also created helper methods.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def format_created_at(obj)
+        date = obj.created_at.to_date
+
+        date.strftime("%b %d, %Y")
+    end
 end
--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,2 +1,29 @@
 module ImagesHelper
+    def index_date_format(image)
+        created = image.created_at.to_datetime
+        now = DateTime.now
+        seconds = ((now - created) * 24 * 60 * 60).to_i
+        minutes = seconds/60
+        hours = minutes/60
+        days = hours/24
+        months = days/30
+        years = months/12
+
+
+        if seconds < 60
+            return "#{seconds} seconds ago."
+        elsif minutes < 60
+            return "#{minutes} minutes ago."    
+        elsif hours < 24
+            return "#{hours} hours ago."
+        elsif days < 30
+            return "#{days} days ago."
+        elsif months < 12
+            return "#{months} months ago."
+        else
+            return "#{years} years ago."
+        end
+
+        
+    end
 end
